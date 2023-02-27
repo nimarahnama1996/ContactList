24a8802c405ce59f414b21c5adecd8932ae91393
nimarahnama1996 <n.rahnama1996@gmail.com>
2023-02-27 14:15:52 +0330

    done!

---
+++ my-app/src/pages/Home.tsx
@@ -0,0 +1,93 @@
+import React, { Fragment, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
+
+
+import "./Home.css";
+import { useContactsQuery,useDeleteContactMutation } from "../services/contactsApi";
+
+
+
+
+const Home = () => {
+
+  const { data, isLoading, error } = useContactsQuery();
+
+  const [deleteContact] = useDeleteContactMutation();
+
+  useEffect(() => {
+   if(error) {
+    toast.error('Something went wrong')
+   }
+  },[error]);
+
+  if (isLoading) {
+    return <h3>Loading...</h3>;
+  }
+
+
+  const handleDelete = async (id: any) => {
+    if (
+      window.confirm("Are you sure that you wanted to delete that contact ?")
+    ) {
+      await deleteContact(id);
+      toast.success("Contact Deleted Successfully");
+    }
+  };
+
+  return (
+   <Fragment>
+
+   <div  style={{ marginTop: "100px" }}>
+   <Link to="/addContact">
+        <button className="btn btn-add">Add Contact</button>
+      </Link>
+      <br />
+      <br />
+      <table className="styled-table">
+
+        <thead>
+          <tr>
+            <th style={{ textAlign: "center" }}>No.</th>
+            <th style={{ textAlign: "center" }}>Name</th>
+            <th style={{ textAlign: "center" }}>Email</th>
+            <th style={{ textAlign: "center" }}>Contact</th>
+            <th style={{ textAlign: "center" }}>Action</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {data?.map((item: any, index: any) => {
+            return (
+              <tr key={item.id}>
+                <th scope="row">{index + 1}</th>
+                <td>{item.name}</td>
+                <td>{item.email}</td>
+                <td>{item.contact}</td>
+                <td>
+                  <Link to={`/editContact/${item.id}`}>
+                    <button className="btn btn-edit">Edit</button>
+                  </Link>
+                  <button
+                    className="btn btn-delete"
+                    onClick={() => handleDelete(item.id)}
+                  >
+                    Delete
+                  </button>
+                  <Link to={`/info/${item.id}`}>
+                    <button className="btn btn-view">View</button>
+                  </Link>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+
+        </table>
+   </div>
+
+   </Fragment>
+  )
+}
+
+export default Home
---
+++ my-app/src/store.tsx
@@ -0,0 +1,15 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+
+
+import { contactsApi } from "./services/contactsApi";
+
+
+
+export const store = configureStore({
+    reducer: {
+        [contactsApi.reducerPath] : contactsApi.reducer
+    },
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware().concat(contactsApi.middleware),
+  });
